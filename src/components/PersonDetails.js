ff157bee615606c54d53522b417885572529a25a
Elvira Fuente <hebebicho@gmail.com>
2019-05-20 19:22:00 +0200

    add persondetail

--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -3,9 +3,18 @@ import React from "react";
 
 class PersonDetails extends React.Component {
     render() {
+        const {info}=this.props
+        if (!info.length){
+            console.log("no hay")
+        }
+         else if (this.props.match.params.id===info[0].login.uuid){
+           return <p>info[0].name</p>
+        }
+        
+
         return (
             <div>
-               <p>  Hola {this.props.match.params.id}</p>
+               <p>  Hola {this.props.match.params.id} </p>
                 {/* <h3>{`${person.name.first} ${person.name.last}`}</h3>
                 <img src={person.picture.medium} alt={`${person.name.first} ${person.name.last}`} />
                 <p>{person.gender}</p>
